12d43253a80bf13f8cffce1fdc132fe8e195dca5
cindrolino <marko.cindric7@gmail.com>
2024-10-24 19:49:12 +0200

    navbar

--- crypto-tracker/src/Components/Navbar/Navbar.jsx
+++ crypto-tracker/src/Components/Navbar/Navbar.jsx
@@ -5,14 +5,13 @@ const Navbar = () =>{
         <header className='header'>
             <div className="nav">
 
-                <div className="nav-logo">EV-olution</div>
-
                 <ul className="nav-menu">
 
                     <li><a href="/">Home</a></li>
                     <li><a href="/about" >About</a></li>
                     <li><a href="/explore">Explore</a></li>
                     <li><a href='/contact' className="nav-contact">Contact</a></li>
+                    <li><input type="text" placeholder='Search'/></li>
 
                 </ul>
 
